78cf57aa917e268d576fc064795ec4d7b1355513
ntakeda67 <n.takeda67@gmail.com>
2013-10-05 13:22:18 +0900

    テスト結果を出力

--- bin/runTest.sh
+++ bin/runTest.sh
@@ -1,3 +1,6 @@
 source ./testEnv.conf
 
-java -jar ${TEST_DRIVER} --tests all --runnerMode DEBUG --basePath ../
+TEST_RESULT_DIR=testOutput
+mkdir -p ${TEST_RESULT_DIR}
+
+java -jar ${TEST_DRIVER} --tests all --runnerMode DEBUG --basePath ../ --testOutput ${TEST_RESULT_DIR}
