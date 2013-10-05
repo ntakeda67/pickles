9358dde094c6a5cb99e3e2bb70fc642df48b8ee4
ntakeda67 <n.takeda67@gmail.com>
2013-10-05 13:10:13 +0900

    runTestServer.sh実行時に、jsTestDriverのpidを保存し、次回実行時にプロセスをkillする(Jenkinsでの繰り返し実行のため)

--- bin/runTestServer.sh
+++ bin/runTestServer.sh
@@ -1,3 +1,10 @@
 source ./testEnv.conf
 
+PID_FILE=js_test_driver.pid
+
+if [ -a ${PID_FILE} ]; then
+     kill `cat ${PID_FILE}`
+fi
+
 java -jar ${TEST_DRIVER} --port 9876 --basePath ../ & 
+echo $! > ${PID_FILE}
